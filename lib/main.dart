e0f587aa807f8eff730b9560c0331fc8e8aa37f1
omidkarami77 <omidtkd77@gmail.com>
2024-11-16 19:27:22 +0330

    add pic and edit header

--- lib/main.dart
+++ lib/main.dart
@@ -15,23 +15,49 @@ class MyApplication extends StatelessWidget {
         fontFamily: 'vazir',
       ),
       home: Scaffold(
-        appBar: getAppBar(),
-        body: SafeArea(child: getMainBody()),
+        appBar: _getAppBar(),
+        body: SafeArea(child: _getMainBody()),
       ),
     );
   }
 }
 
-Widget getMainBody() {
+Widget _getMainBody() {
   return SingleChildScrollView(
-    child: Column(),
+    child: Column(
+      children: [
+        getHeader(),
+      ],
+    ),
   );
 }
 
-PreferredSizeWidget getAppBar() {
-  return AppBar(
-    elevation: 5,
-    backgroundColor: Colors.blue,
-    title: Text("Omi karami"),
+Widget getHeader() {
+  return const Column(
+    children: [
+      SizedBox(
+        height: 20,
+      ),
+      CircleAvatar(
+        radius: 70,
+        backgroundImage: AssetImage("images/omid.jpg"),
+      ),
+      Text(
+        "امیدم یک برنامه نویس و تحلیلگر",
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w900,
+        ),
+      ),
+      SizedBox(
+        height: 15,
+      ),
+      Text("عاشق برنامه نویسی موبایل و بازارهای مالی")
+    ],
   );
+}
+
+PreferredSizeWidget _getAppBar() {
+  return AppBar(
+      elevation: 5, backgroundColor: Colors.blue, title: Text("hello"));
 }
