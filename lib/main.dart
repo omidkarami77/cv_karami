4107f6ef1e6edda221b8ac2674bb2cf400f37dbd
omidkarami77 <omidtkd77@gmail.com>
2024-11-17 16:58:40 +0330

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,22 @@ class MyApplication extends StatelessWidget {
 Widget _getMainBody() {
   return SingleChildScrollView(
     child: Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        SizedBox(
+          width: double.infinity,
+        ),
         getHeader(),
+        _getRowIcons(),
+        SizedBox(
+          height: 12,
+        ),
+        _getSkillLabels(),
+        SizedBox(
+          height: 12,
+        ),
+        getResume(),
       ],
     ),
   );
@@ -62,44 +75,108 @@ Widget getHeader() {
       SizedBox(
         height: 10,
       ),
-      _getRowIcons(),
     ],
   );
 }
 
 Widget _getRowIcons() {
+  final icons = [
+    FontAwesomeIcons.instagram,
+    FontAwesomeIcons.linkedin,
+    FontAwesomeIcons.whatsapp,
+    FontAwesomeIcons.twitter,
+  ];
+
   return Wrap(
     runSpacing: 20,
     spacing: 20,
     alignment: WrapAlignment.center,
-    children: [
-      IconButton(
-          onPressed: () {}, icon: const FaIcon(FontAwesomeIcons.accusoft)),
-      IconButton(
-          onPressed: () {}, icon: const FaIcon(FontAwesomeIcons.accusoft)),
-      IconButton(
-          onPressed: () {}, icon: const FaIcon(FontAwesomeIcons.accusoft)),
-      IconButton(
-          onPressed: () {}, icon: const FaIcon(FontAwesomeIcons.accusoft)),
-      IconButton(
-          onPressed: () {}, icon: const FaIcon(FontAwesomeIcons.accusoft)),
-      IconButton(
-          onPressed: () {}, icon: const FaIcon(FontAwesomeIcons.accusoft)),
-      IconButton(
-          onPressed: () {}, icon: const FaIcon(FontAwesomeIcons.accusoft)),
-      IconButton(
-          onPressed: () {}, icon: const FaIcon(FontAwesomeIcons.accusoft)),
-      IconButton(
-          onPressed: () {}, icon: const FaIcon(FontAwesomeIcons.accusoft)),
-      IconButton(
-          onPressed: () {}, icon: const FaIcon(FontAwesomeIcons.accusoft)),
-      IconButton(
-          onPressed: () {}, icon: const FaIcon(FontAwesomeIcons.accusoft)),
-    ],
+    children: icons
+        .map((icon) => IconButton(
+              onPressed: () {},
+              icon: FaIcon(icon),
+            ))
+        .toList(),
   );
 }
 
 PreferredSizeWidget _getAppBar() {
   return AppBar(
-      elevation: 5, backgroundColor: Colors.blue, title: Text("hello"));
+    elevation: 5,
+    centerTitle: true,
+    backgroundColor: Colors.blue,
+    title: Text(
+      "امید کرمی",
+      textAlign: TextAlign.center,
+    ),
+  );
+}
+
+Widget _getSkillLabels() {
+  final skills = [
+    {'image': 'images/android.png', 'label': 'android'},
+    {'image': 'images/flutter.png', 'label': 'flutter'},
+    {'image': 'images/api.png', 'label': 'API'},
+    {'image': 'images/dart.png', 'label': 'Dart'},
+    {'image': 'images/java.png', 'label': 'Java'},
+    {'image': 'images/linux.png', 'label': 'linux'}
+  ];
+
+  return SingleChildScrollView(
+    scrollDirection: Axis.horizontal,
+    child: Row(
+      children: skills.map((skill) {
+        return Card(
+          elevation: 6,
+          child: Column(
+            children: [
+              Container(
+                width: 80,
+                height: 80,
+                child: Image.asset(skill['image']!),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(skill['label']!),
+              ),
+            ],
+          ),
+        );
+      }).toList(),
+    ),
+  );
+}
+
+Widget getResume() {
+  final experiences = [
+    "برنامه نویس جاوا در شرکت داتین سال 99",
+    "برنامه نویس موبایل در فناوری اطلاعات رجا سال 1400 الی 1401",
+    "مدیر و موسس نوید ترابر آسیا "
+  ];
+
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+    width: double.infinity,
+    color: Colors.grey[200],
+    child: Column(
+      children: [
+        Text(
+          "سابقه کاری من",
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisSize: MainAxisSize.min,
+          children: experiences
+              .map((experience) => Text(
+                    experience,
+                    textAlign: TextAlign.right,
+                    textDirection: TextDirection.rtl,
+                  ))
+              .toList(),
+        ),
+      ],
+    ),
+  );
 }
